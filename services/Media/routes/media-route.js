fb1acbf82e6052daea86e1593a2fe78a99cd584b
Habib <habibali6.ha@gmail.com>
2021-09-23 15:28:17 +0100

    route for giving number of files and names

--- services/Media/routes/media-route.js
+++ services/Media/routes/media-route.js
@@ -1,8 +1,25 @@
 const router = require('express').Router()
 const media = require('../media')
+const fs = require('fs')
+const { promisify } = require('util')
 
 router.get('/media/logo', (req, res) => {
     res.sendFile(process.cwd() + "/resources/iqra-logo.png")
+})
+
+router.get('/media/slides', async (req, res) => {
+    fs.readdir("./resources/slides", (err, files) => {
+        try {
+            let numOfFiles = files.length
+            res.json({
+                "numOfFiles" : numOfFiles,
+                "files" : files
+            })
+        } catch(e) {
+            console.error(e)
+            res.json({ error : "There has been an error: " + e })
+        }
+    });
 })
 
 
