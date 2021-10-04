874153b02016d74eb86679d022b5a6bdff153434
Habib <habibali6.ha@gmail.com>
2021-10-04 14:31:22 +0100

    select slide route added

--- services/Media/routes/media-route.js
+++ services/Media/routes/media-route.js
@@ -22,5 +22,14 @@ router.get('/media/slides', async (req, res) => {
     });
 })
 
+router.get('/media/slides/:id', async (req, res) => {
+    let id = req.params.id
+    try {
+        res.sendFile(process.cwd() + `/resources/slides/slide${id}.jpg`)
+    } catch(e) {
+        console.log("There has been an error with this request: " + e)
+    }
+})
+
 
 module.exports = router
