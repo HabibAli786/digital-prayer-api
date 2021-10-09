9b1df2a4dad7b26d8576d6e90ee133a955c0d408
Habib <habibali6.ha@gmail.com>
2021-10-09 14:29:22 +0100

    register route made with hashed password

--- services/Admin/routes/admin-route.js
+++ services/Admin/routes/admin-route.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 const sqlite = require('sqlite3').verbose()
+const bcrypt = require('bcrypt');
+const { reset } = require('nodemon');
 
 const admin = require('../admin')
+
+const saltRounds = 10;
 
 let db = new sqlite.Database('./services/Admin/admin.db', (err) => {
     if(err) {
@@ -13,15 +17,42 @@ let db = new sqlite.Database('./services/Admin/admin.db', (err) => {
 router.post('/admin/register', (req, res) => {
   const username = req.body.username
   const password = req.body.password
-  console.log(username)
-  console.log(password)
-  const result = db.get('SELECT * FROM admin', (err, results) => {
-    if(err) {
-      console.log(err)
-    } else {
-      console.log(results)
-    }
+  // console.log(username)
+  // console.log(password)
+  db.serialize(() => {
+    db.get(`SELECT username FROM admin WHERE username="${username}"`, async (err, result) => {
+      if(err) {
+        throw new Error(err)
+      }
+      if(result) {
+        throw new Error("this username exists already in the database")
+      } else {
+        console.log("this username does not exist")
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
+        console.log(hashedPassword)
+
+        db.run(`INSERT INTO admin (username, password) VALUES ('${username}', '${hashedPassword}');`, (err, result) => {
+          if(err) {
+            throw new Error(err)
+          } else {
+            res.send("Account succesfully made!")
+          }
+        })
+
+        // const match = await bcrypt.compare(testMatch, hashedPassword);
+        // console.log(match)
+      }
+    })
+
+    // const result = db.get('SELECT * FROM admin', (err, results) => {
+    //   if(err) {
+    //     console.log(err)
+    //   } else {
+    //     console.log(results)
+    //   }
+    // })
   })
+  // db.close()
 })
 
 router.get('/admin/login', (req, res) => {
