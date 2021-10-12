2f2a4aa6a14d638055daf4e012948bd83eabe102
Habib <habibali6.ha@gmail.com>
2021-10-12 11:17:59 +0100

    login route created

--- services/Admin/routes/admin-route.js
+++ services/Admin/routes/admin-route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const sqlite = require('sqlite3').verbose()
 const bcrypt = require('bcrypt');
+const { response } = require('express');
 const { reset } = require('nodemon');
 
 const admin = require('../admin')
@@ -22,9 +23,11 @@ router.post('/admin/register', (req, res) => {
   db.serialize(() => {
     db.get(`SELECT username FROM admin WHERE username="${username}"`, async (err, result) => {
       if(err) {
+        res.send(err)
         throw new Error(err)
       }
       if(result) {
+        res.send(result)
         throw new Error("this username exists already in the database")
       } else {
         console.log("this username does not exist")
@@ -38,9 +41,6 @@ router.post('/admin/register', (req, res) => {
             res.send("Account succesfully made!")
           }
         })
-
-        // const match = await bcrypt.compare(testMatch, hashedPassword);
-        // console.log(match)
       }
     })
 
@@ -55,8 +55,33 @@ router.post('/admin/register', (req, res) => {
   // db.close()
 })
 
-router.get('/admin/login', (req, res) => {
-  console.log(req.body)
+router.post('/admin/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  db.serialize(() => {
+    db.get(`SELECT username, password FROM admin WHERE username="${username}";`, async (err, result) => {
+      try {
+        // if(err) {
+        //   throw new Error(err)
+        // }      
+        const dbUsername = result.username
+        const dbPassword = result.password
+        console.log(dbUsername)
+        console.log(dbPassword)
+
+        const match = await bcrypt.compare(password, dbPassword)
+
+        if(match) {
+          res.send("Passwords match")
+        } else {
+          res.send("Password is incorrect")
+        }
+      } catch(e) {
+        console.log(e)
+        res.send("The username is does not exist")
+      }
+    })
+  })
 })
 
   
